955f250bc4646044dddfe4ba5919c153d8095dce
dekimasoon <dekimasoon@gmail.com>
2022-11-10 02:48:37 +0000

    add the caddy module

---
+++ caddy/upstreams.go
@@ -0,0 +1,88 @@
+package upstreams
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp/reverseproxy"
+	"go.uber.org/zap"
+)
+
+func init() {
+	caddy.RegisterModule(K8sNodeUpstreams{})
+}
+
+type K8sNodeUpstreams struct {
+	FilterLabel string `json:"filter_label,omitempty"`
+	
+	logger     *zap.Logger
+}
+
+// CaddyModule returns the Caddy module information.
+func (K8sNodeUpstreams) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.reverse_proxy.upstreams.k8s_node",
+		New: func() caddy.Module { return new(K8sNodeUpstreams) },
+	}
+}
+
+// Provision sets up the module.
+func (u *K8sNodeUpstreams) Provision(ctx caddy.Context) error {
+	u.logger = ctx.Logger(u)
+
+	return nil
+}
+
+func (u K8sNodeUpstreams) GetUpstreams(r *http.Request) ([]*reverseproxy.Upstream, error) {
+	var upstreams []*reverseproxy.Upstream
+
+	upstreams = append(upstreams, &reverseproxy.Upstream{
+		Dial: "10.128.0.31:32080",
+	})
+	return upstreams, nil
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler. Syntax:
+//
+//	dynamic k8s_node {
+//		filter_label	<filter_label>
+//	}
+func (u *K8sNodeUpstreams) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		args := d.RemainingArgs()
+		u.logger.Info("RemainingArgs: " + strconv.Itoa(len(args)))
+
+		if len(args) > 0 {
+			return d.ArgErr()
+		}
+
+		for d.NextBlock(0) {
+			u.logger.Info("first d.Val(): " + d.Val())
+			switch d.Val() {
+			case "filter_label":
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				if u.FilterLabel != "" {
+					return d.Errf("srv filter label has already been specified")
+				}
+				u.logger.Info("next d.Val(): " + d.Val())
+				u.FilterLabel = d.Val()
+			}
+		}
+	}
+	return nil
+}
+
+func (u K8sNodeUpstreams) String() string {
+	return "k8s_node_upstream"
+}
+
+// Interface guards
+var (
+	_ caddy.Provisioner           = (*K8sNodeUpstreams)(nil)
+	_ reverseproxy.UpstreamSource = (*K8sNodeUpstreams)(nil)
+	_ caddyfile.Unmarshaler       = (*K8sNodeUpstreams)(nil)
+)
